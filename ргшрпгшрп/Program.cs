c85bc28774a6ed36a7fcbaf5cfe25208198779c2
Sofiia Koliada <koliadasonia@gmail.com>
2024-11-25 22:46:45 +0200

    Add new feature to calculate total" або "Fix bug in login module

---
+++ ргшрпгшрп/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        // Оголошення масиву чисел
+        int[] numbers = { 5, 10, 15, 20, 25 };
+
+        // Виклик методу для обчислення суми
+        int sum = CalculateSum(numbers);
+
+        // Вивід результату
+        Console.WriteLine($"Сума чисел у масиві: {sum}");
+    }
+
+    // Метод для обчислення суми елементів масиву
+    static int CalculateSum(int[] array)
+    {
+        int sum = 0;
+        foreach (int num in array)
+        {
+            sum += num;
+        }
+        return sum;
+    }
+}
